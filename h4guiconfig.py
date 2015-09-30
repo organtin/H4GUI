8c36d7b75b2e6ce599a01caedb284470bd22b730
cmsdaq <cmsroma.daq@gmail.com>
2015-09-30 17:45:32 +0200

    2 crate operation for October 2015 tests

--- h4guiconfig.py
+++ h4guiconfig.py
@@ -13,7 +13,7 @@ def configure(self):
 
     self.nodes=[ # addresses of connected nodes
         ('RC','tcp://pcethtb2.cern.ch:6002'),
-        #('RO1','tcp://pcethtb1.cern.ch:6002'),
+        ('RO1','tcp://pcethtb1.cern.ch:6002'),
         #        ('RO2','tcp://cms-h4-03:6002'),
         ('EVTB','tcp://pcethtb2.cern.ch:6502'),
         ('DRCV1','tcp://cms-h4-04.cern.ch:6502'),
@@ -23,7 +23,7 @@ def configure(self):
 
     self.keepalive={} # nodes to monitor (comment to remove, never put False)
     self.keepalive['RC']=True
-#    self.keepalive['RO1']=True
+    self.keepalive['RO1']=True
 #    self.keepalive['RO2']=False
     self.keepalive['EVTB']=True
     self.keepalive['DRCV1']=True
@@ -44,7 +44,7 @@ def configure(self):
         'free_space': None,
 #        'start_daemons': '../H4DAQ/scripts/startall.sh -v3 --rc=pcethtb2 --eb=pcethtb2 --dr=pcethtb1',
 #        'start_daemons': '../H4DAQ/scripts/startall.sh -v3 --rc=pcethtb2 --eb=pcethtb2 --dr=pcethtb1',
-        'start_daemons': '../H4DAQ/scripts/startall.sh -v3 --rc=pcethtb2 --eb=pcethtb2 --drcv=cms-h4-04,cms-h4-05 --drcvrecompile',
+        'start_daemons': '../H4DAQ/scripts/startall.sh -v3 --rc=pcethtb2 --dr=pcethtb1 --eb=pcethtb2 --drcv=cms-h4-04,cms-h4-05 --drcvrecompile',
 #        'start_daemons': '../H4DAQ/scripts/startall.sh -v3 --rc=pcethtb2 --eb=pcethtb1 --dr=pcethtb1 --drcv=cms-h4-05 --drcvrecompile',
 #        'start_daemons': '../H4DAQ/scripts/startall.sh -v3 --rc=pcethtb2 --eb=pcethtb2 --dr=pcethtb1',
 #        'start_daemons': '../H4DAQ/scripts/startall.sh -v3 --rc=pcethtb2 --dr=pcethtb1 --drcv=cms-h4-04,cms-h4-05 --drcvrecompile',
@@ -100,55 +100,118 @@ def configure(self):
 #    self.tableposdictionary['CEF3_10deg_-10(Y)']=(166.5,264)
 #
 
-    self.tableposdictionary['DRMATRIX_CENTER']=(196.5,248.0)
-    self.tableposdictionary['XTALS_7']= (167.0,218.5)
-    self.tableposdictionary['XTALS_8']= (196.5,218.5)
-    self.tableposdictionary['XTALS_9']= (226.0,218.5)
-    self.tableposdictionary['XTALS_12']= (167.0,248.0)
-    self.tableposdictionary['XTALS_13']= (196.5,248.0)
-    self.tableposdictionary['XTALS_14']= (226.0,248.0)
-    self.tableposdictionary['XTALS_17']= (167.0,277.5)
-    self.tableposdictionary['XTALS_18']= (196.5,277.5)
-    self.tableposdictionary['XTALS_19']= (226.0,277.5)
+#MAY DRO TB
+#    self.tableposdictionary['DRMATRIX_CENTER']=(196.5,248.0)
+#    self.tableposdictionary['XTALS_7']= (167.0,218.5)
+#    self.tableposdictionary['XTALS_8']= (196.5,218.5)
+#    self.tableposdictionary['XTALS_9']= (226.0,218.5)
+#    self.tableposdictionary['XTALS_12']= (167.0,248.0)
+#    self.tableposdictionary['XTALS_13']= (196.5,248.0)
+#    self.tableposdictionary['XTALS_14']= (226.0,248.0)
+#    self.tableposdictionary['XTALS_17']= (167.0,277.5)
+#    self.tableposdictionary['XTALS_18']= (196.5,277.5)
+#    self.tableposdictionary['XTALS_19']= (226.0,277.5)
+
+#OCT DRO TB
+#    self.tableposdictionary['DRMATRIX_CENTER']=(198.7,249.9)
+#    self.tableposdictionary['XTALS_7']= (169.2,220.4)
+#    self.tableposdictionary['XTALS_8']= (198.7,220.4)
+#    self.tableposdictionary['XTALS_9']= (228.2,220.4)
+#    self.tableposdictionary['XTALS_12']= (169.2,249.9)
+#    self.tableposdictionary['XTALS_13']= (198.7,249.9)
+#    self.tableposdictionary['XTALS_14']= (228.2,249.9)
+#    self.tableposdictionary['XTALS_17']= (169.2,279.4)
+#    self.tableposdictionary['XTALS_18']= (198.7,279.4)
+#    self.tableposdictionary['XTALS_19']= (228.2,279.4)
+
+#SEP SPACAL TB
+    self.tableposdictionary['MATRIX_CENTER']=(200.2,340.0)
+    self.tableposdictionary['CH_0']= (180.2,320.0)
+    self.tableposdictionary['CH_1']= (200.2,320.0)
+    self.tableposdictionary['CH_2']= (220.2,320.0)
+    self.tableposdictionary['CH_3']= (180.2,340.0)
+    self.tableposdictionary['CH_4']= (200.2,340.0)
+    self.tableposdictionary['CH_5']= (220.2,340.0)
+    self.tableposdictionary['CH_6']= (180.2,360.0)
+    self.tableposdictionary['CH_7']= (200.2,360.0)
+    self.tableposdictionary['CH_8']= (220.2,360.0)
+  
+#SPACAL OCT TRANSVERSE SCAN
+    self.tableposdictionary['top_1']= (100.2,320.0)
+    self.tableposdictionary['top_2']= (120.2,320.0)
+    self.tableposdictionary['top_3']= (140.2,320.0)
+    self.tableposdictionary['top_4']= (160.2,320.0)
+    self.tableposdictionary['top_5']= (180.2,320.0)
+    self.tableposdictionary['top_6']= (200.2,320.0)
+    self.tableposdictionary['top_7']= (220.2,320.0)
+    self.tableposdictionary['top_8']= (240.2,320.0)
+    self.tableposdictionary['top_9']= (260.2,320.0)
+    self.tableposdictionary['top_10']= (280.2,320.0)
+    self.tableposdictionary['top_11']= (300.2,320.0)
+
+    self.tableposdictionary['mid_1']= (100.2,340.0)
+    self.tableposdictionary['mid_2']= (120.2,340.0)
+    self.tableposdictionary['mid_3']= (140.2,340.0)
+    self.tableposdictionary['mid_4']= (160.2,340.0)
+    self.tableposdictionary['mid_5']= (180.2,340.0)
+    self.tableposdictionary['mid_6']= (200.2,340.0)
+    self.tableposdictionary['mid_7']= (220.2,340.0)
+    self.tableposdictionary['mid_8']= (240.2,340.0)
+    self.tableposdictionary['mid_9']= (260.2,340.0)
+    self.tableposdictionary['mid_10']= (280.2,340.0)
+    self.tableposdictionary['mid_11']= (300.2,340.0)
+
+    self.tableposdictionary['bot_1']= (100.2,360.0)
+    self.tableposdictionary['bot_2']= (120.2,360.0)
+    self.tableposdictionary['bot_3']= (140.2,360.0)
+    self.tableposdictionary['bot_4']= (160.2,360.0)
+    self.tableposdictionary['bot_5']= (180.2,360.0)
+    self.tableposdictionary['bot_6']= (200.2,360.0)
+    self.tableposdictionary['bot_7']= (220.2,360.0)
+    self.tableposdictionary['bot_8']= (240.2,360.0)
+    self.tableposdictionary['bot_9']= (260.2,360.0)
+    self.tableposdictionary['bot_10']= (280.2,360.0)
+    self.tableposdictionary['bot_11']= (300.2,360.0)
+
+#DRO MAY TEST 
+#    self.tableposdictionary['top_1']= (92.5,218.5)
+#    self.tableposdictionary['top_2']= (112.5,218.5)
+#    self.tableposdictionary['top_3']= (132.5,218.5)
+#    self.tableposdictionary['top_4']= (152.5,218.5)
+#    self.tableposdictionary['top_5']= (172.5,218.5)
+#    self.tableposdictionary['top_6']= (192.5,218.5)
+#    self.tableposdictionary['top_7']= (212.5,218.5)
+#    self.tableposdictionary['top_8']= (232.5,218.5)
+#    self.tableposdictionary['top_9']= (252.5,218.5)
+#    self.tableposdictionary['top_10']= (272.5,218.5)
+#    self.tableposdictionary['top_11']= (292.5,218.5)
+#    self.tableposdictionary['top_12']= (312.5,218.5)
     
-    self.tableposdictionary['top_1']= (92.5,218.5)
-    self.tableposdictionary['top_2']= (112.5,218.5)
-    self.tableposdictionary['top_3']= (132.5,218.5)
-    self.tableposdictionary['top_4']= (152.5,218.5)
-    self.tableposdictionary['top_5']= (172.5,218.5)
-    self.tableposdictionary['top_6']= (192.5,218.5)
-    self.tableposdictionary['top_7']= (212.5,218.5)
-    self.tableposdictionary['top_8']= (232.5,218.5)
-    self.tableposdictionary['top_9']= (252.5,218.5)
-    self.tableposdictionary['top_10']= (272.5,218.5)
-    self.tableposdictionary['top_11']= (292.5,218.5)
-    self.tableposdictionary['top_12']= (312.5,218.5)
+#    self.tableposdictionary['bottom_1']= (92.5,277.5)
+#    self.tableposdictionary['bottom_2']= (112.5,277.5)
+#    self.tableposdictionary['bottom_3']= (132.5,277.5)
+#    self.tableposdictionary['bottom_4']= (152.5,277.5)
+#    self.tableposdictionary['bottom_5']= (172.5,277.5)
+#    self.tableposdictionary['bottom_6']= (192.5,277.5)
+#    self.tableposdictionary['bottom_7']= (212.5,277.5)
+#    self.tableposdictionary['bottom_8']= (232.5,277.5)
+#    self.tableposdictionary['bottom_9']= (252.5,277.5)
+#    self.tableposdictionary['bottom_10']= (272.5,277.5)
+#    self.tableposdictionary['bottom_11']= (292.5,277.5)
+#    self.tableposdictionary['bottom_12']= (312.5,277.5)
     
-    self.tableposdictionary['bottom_1']= (92.5,277.5)
-    self.tableposdictionary['bottom_2']= (112.5,277.5)
-    self.tableposdictionary['bottom_3']= (132.5,277.5)
-    self.tableposdictionary['bottom_4']= (152.5,277.5)
-    self.tableposdictionary['bottom_5']= (172.5,277.5)
-    self.tableposdictionary['bottom_6']= (192.5,277.5)
-    self.tableposdictionary['bottom_7']= (212.5,277.5)
-    self.tableposdictionary['bottom_8']= (232.5,277.5)
-    self.tableposdictionary['bottom_9']= (252.5,277.5)
-    self.tableposdictionary['bottom_10']= (272.5,277.5)
-    self.tableposdictionary['bottom_11']= (292.5,277.5)
-    self.tableposdictionary['bottom_12']= (312.5,277.5)
-    
-    self.tableposdictionary['middle_1']= (92.5,248)
-    self.tableposdictionary['middle_2']= (112.5,248)
-    self.tableposdictionary['middle_3']= (132.5,248)
-    self.tableposdictionary['middle_4']= (152.5,248)
-    self.tableposdictionary['middle_5']= (172.5,248)
-    self.tableposdictionary['middle_6']= (192.5,248)
-    self.tableposdictionary['middle_7']= (212.5,248)
-    self.tableposdictionary['middle_8']= (232.5,248)
-    self.tableposdictionary['middle_9']= (252.5,248)
-    self.tableposdictionary['middle_10']= (272.5,248)
-    self.tableposdictionary['middle_11']= (292.5,248)
-    self.tableposdictionary['middle_12']= (312.5,248)
+#    self.tableposdictionary['middle_1']= (92.5,248)
+#    self.tableposdictionary['middle_2']= (112.5,248)
+#    self.tableposdictionary['middle_3']= (132.5,248)
+#    self.tableposdictionary['middle_4']= (152.5,248)
+#    self.tableposdictionary['middle_5']= (172.5,248)
+#    self.tableposdictionary['middle_6']= (192.5,248)
+#    self.tableposdictionary['middle_7']= (212.5,248)
+#    self.tableposdictionary['middle_8']= (232.5,248)
+#    self.tableposdictionary['middle_9']= (252.5,248)
+#    self.tableposdictionary['middle_10']= (272.5,248)
+#    self.tableposdictionary['middle_11']= (292.5,248)
+#    self.tableposdictionary['middle_12']= (312.5,248)
     
     
     otherxtals = OrderedDict() # coordinates seen from the rear face
